1678c04f4376068df82bef022c7fbdfd53769310
shuhaibw <120868621+shuhaibw@users.noreply.github.com>
2024-09-26 01:08:52 -0700

    update1

--- lab15.cpp
+++ lab15.cpp
@@ -1,1 +1,43 @@
 // COMSC-210 | Lab 15 | Shuhaib Walahi
+
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Movie
+{
+    private:
+        string title;
+        int year;
+        string screenwriter;
+    public:
+
+        // setters
+        void setTitle(const string& t) {title = t;} // string& avoids copy of string
+        void setYear(int y) {year = y;}
+        void setScreenwriter(const string& s) {screenwriter = s;}
+
+        // getters
+        string getTitle() const {return title;}
+        int getYear() const {return year;}
+        string getScreenwriter() const {return screenwriter;}
+
+        // print the movie
+        void print() const
+        {
+            cout << "Movie: " << title << endl;
+            cout << setw(19) << "Year released: " << year << endl;
+            cout << setw(18) << "Screenwriter: " << screenwriter << endl << endl;
+        }
+};
+
+int main()
+{
+    // vector to hold the movie objects
+    vector<Movie> movies;
+
+    return 0;
+}
