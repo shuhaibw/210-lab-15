f405e9d7c4815b93cfc51f3c750f352e6710030b
shuhaibw <120868621+shuhaibw@users.noreply.github.com>
2024-09-26 01:19:06 -0700

    complete

--- lab15.cpp
+++ lab15.cpp
@@ -4,6 +4,7 @@
 #include <iomanip>
 #include <vector>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
@@ -14,7 +15,6 @@ class Movie
         int year;
         string screenwriter;
     public:
-
         // setters
         void setTitle(const string& t) {title = t;} // string& avoids copy of string
         void setYear(int y) {year = y;}
@@ -38,6 +38,50 @@ int main()
 {
     // vector to hold the movie objects
     vector<Movie> movies;
+
+    ifstream fin ("../input.txt");
+    // var to hold temporary title
+    string title;
+    // var to hold temporary year
+    int year;
+    // var to hold temporary screenwriter
+    string screenwriter;
+    // temporary index
+    int i = 0;
+
+    if (fin.good())
+    {
+        while (getline(fin, title))
+        {
+            fin >> year;
+            // ignore the newline after the year
+            fin.ignore();
+            getline(fin, screenwriter);
+
+            // declare the temp movie object
+            Movie movie;
+
+            // populate the movie object
+            movie.setTitle(title);
+            movie.setYear(year);
+            movie.setScreenwriter(screenwriter);
+
+            // add all temp movie objects to vector
+            movies.push_back(movie);
+            i++;
+        }
+        fin.close();
+    }
+    else
+    {
+        cout << "Error: unable to open file." << endl;
+    }
+
+    // print the movies in vector
+    for (auto val: movies)
+    {
+        val.print();
+    }
 
     return 0;
 }
